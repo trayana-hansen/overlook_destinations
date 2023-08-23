606df026a395bc02d0b4e993d00870fdef965189
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2023-08-23 23:25:18 +0200

    carousel_finished

--- src/Components/Pages/Home/Slider/Slider.jsx
+++ src/Components/Pages/Home/Slider/Slider.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
+import "./Slider.scss"
 
 const handleDragStart = (e) => e.preventDefault();
 
