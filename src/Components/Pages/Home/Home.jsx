1d3acb5c9695e6acda02faced18b93feceec5c19
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2023-08-23 22:22:42 +0200

    slider, middle ground working

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Home.scss";
+import Slider from "./Slider/Slider";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Slider />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/Components/Pages/Home/Slider/Slider.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+
+const handleDragStart = (e) => e.preventDefault();
+
+const items = [
+  <img
+    src={require("../../../../Assets/Images/center-square-wroclaw.jpg")}
+    onDragStart={handleDragStart}
+    alt="center-square-wroclaw"
+    className="sliderImg"
+  />,
+  <img
+    src={require("../../../../Assets/Images/city-houses-reykjavik.jpg")}
+    onDragStart={handleDragStart}
+    alt="city-houses-reykjavik"
+    className="sliderImg"
+  />,
+  <img
+    src={require("../../../../Assets/Images/fishmarket-hamborg.jpg")}
+    onDragStart={handleDragStart}
+    alt="fishmarket-hamborg"
+    className="sliderImg"
+  />,
+  <img
+    src={require("../../../../Assets/Images/frankfurt-skyline-germany.jpg")}
+    onDragStart={handleDragStart}
+    alt="frankfurt-skyline-germany"
+    className="sliderImg"
+  />,
+  <img
+    src={require("../../../../Assets/Images/gdansk-center-church-poland.jpg")}
+    onDragStart={handleDragStart}
+    alt="gdansk-center-church-poland"
+    className="sliderImg"
+  />,
+];
+
+const Slider = () => {
+  return (
+    <AliceCarousel
+      mouseTracking
+      autoHeight="true"
+      autoPlay="true"
+      autoPlayInterval="1200"
+      disableDotsControls="true"
+      infinite="true"
+      keyboardNavigation="true"
+      items={items}
+    />
+  );
+};
+
+export default Slider;
