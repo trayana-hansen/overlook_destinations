65c01f249652339bd9661dfd973840ab787e4421
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2023-08-29 14:43:11 +0200

    some css added

--- src/Components/Pages/NotFound/NotFound.jsx
+++ src/Components/Pages/NotFound/NotFound.jsx
@@ -1,13 +1,20 @@
 import { React, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+//TODO write how many seconds to the front page
 const NotFound = () => {
-    {/* Creating a variable to use as useNavigate hook */}
+  {
+    /* Creating a variable to use as useNavigate hook */
+  }
   const navigate = useNavigate();
   useEffect(() => {
-      {/* Setting a timer before the navigation takes place */}
+    {
+      /* Setting a timer before the navigation takes place */
+    }
     setTimeout(() => {
-        {/* Rerouting to home page after the times is done */}
+      {
+        /* Rerouting to home page after the times is done */
+      }
       navigate("/");
     }, 2000);
   }, []);
